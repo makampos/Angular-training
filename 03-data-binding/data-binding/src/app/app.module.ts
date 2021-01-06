c6401021856647a228ddb524e52cc5b6b890b6eb
makampos <matheus.campos@edu.unifil.br>
2021-01-06 14:00:26 -0300

    Event-binding

--- 03-data-binding/data-binding/src/app/app.module.ts
+++ 03-data-binding/data-binding/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,

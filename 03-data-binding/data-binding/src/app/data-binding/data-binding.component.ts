3e3edb73f62e2e17c01363a58f94a230a52f79c2
makampos <matheus.campos@edu.unifil.br>
2021-01-06 10:12:49 -0300

    Class e Style binding

---
+++ 03-data-binding/data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://mg7.com'
+  cursoAngular: boolean = true;
+
+  urlImagem: string = 'https://i.picsum.photos/id/446/200/300.jpg?hmac=VaF-vrs-svfPoYxxsTKI08lxXNlPBzqyj97M82eX8mc'
+  
+  getValor(){
+    return 1;
+  }
+
+  getCurtirAngular(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
